8983d46152c01d0c291f7cc86b2420a92660bb43
nikila-saravanan <nikila.saravanan@gmail.com>
2015-08-03 10:12:47 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
+
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |element|
+    if new_hash.keys.include?(element)
+      new_hash[element] += 1
+    else
+      new_hash[element] = 1
+    end
+  end
+  new_hash
 end
  
